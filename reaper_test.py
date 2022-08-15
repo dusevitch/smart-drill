7d85cb12e330a7c62b93461b5a460f0433a5cf8b
David <dusevitch@gmail.com>
2022-08-15 14:58:38 -0400

    added CAD files, other reaper test updates

--- reaper_test.py
+++ reaper_test.py
@@ -5,8 +5,20 @@ import reapy
 # Connect to current project
 project = reapy.Project()
 
+
+
 # add a new track
-project.add_track(name="drill_sound")
+project.add_track(index=0, name="drill_sound")
+project.add_track(index=2, name="drill_sound2")
+project.add_track(index=10, name="drill_sound1")
+# reapy.arm_command(project.command_id, section="")
 
 
-project.record()
+tracks = project.tracks
+
+print("TRACK NAMES:\n")
+for track in tracks:
+    print(track.name)
+
+print('disarm all')
+project.disarm_rec_on_all_tracks()
